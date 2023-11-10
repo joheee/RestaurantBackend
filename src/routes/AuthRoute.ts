9e2f84d08786154a86738a8ba592e91b9198770c
Johevin Blesstowi <BS22-1@slc.net>
2023-11-10 14:57:07 +0700

    register all controller to route

--- src/routes/AuthRoute.ts
+++ src/routes/AuthRoute.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { AuthController } from '../controller/AuthController';
 
 export const AuthRoute: Router = express.Router();
+
 AuthRoute.post('/', AuthController.handleLogin)
 AuthRoute.patch('/', AuthController.handleRegister)
 AuthRoute.delete('/', AuthController.handleLogout)
