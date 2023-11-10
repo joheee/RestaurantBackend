3939c9a691bf98fbbe1d32bb38e73fb543f7e09b
Johevin Blesstowi <BS22-1@slc.net>
2023-11-10 11:32:48 +0700

    bug: external route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'
+import { AuthRoute } from 'route/AuthRoute';
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+
+app.use(cors())
+app.use('/auth', AuthRoute)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express Restaurant with Typescript!!');
